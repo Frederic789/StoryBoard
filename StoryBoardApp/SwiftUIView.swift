bed881d0cd2b0bb5d357650fbe3aac73d8cbb9f3
Frederic789 <113332830+Frederic789@users.noreply.github.com>
2023-12-10 08:56:39 -0800

    Update SwiftUIView.swift

--- StoryBoardApp/SwiftUIView.swift
+++ StoryBoardApp/SwiftUIView.swift
@@ -6,13 +6,49 @@
 //
 
 import SwiftUI
+import WebKit
+import PDFKit
 
-struct SwiftUIView: View {
+struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            PDFViewerView()
+                .tabItem {
+                    Label("PDF", systemImage: "doc.text")
+                }
+            WebView(url: URL(string: "https://www.example.com")!)
+                .tabItem {
+                    Label("Webpage", systemImage: "globe")
+                }
+        }
     }
 }
 
-#Preview {
-    SwiftUIView()
+struct PDFViewerView: UIViewRepresentable {
+    func makeUIView(context: Context) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.autoScales = true
+        if let url = Bundle.main.url(forResource: "your-pdf-file", withExtension: "pdf"),
+           let pdfDocument = PDFDocument(url: url) {
+            pdfView.document = pdfDocument
+        }
+        return pdfView
+    }
+
+    func updateUIView(_ uiView: PDFView, context: Context) {
+        // Update the view.
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
 }
